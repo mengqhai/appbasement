a9d0cec8dcfef6e84163dbbc6cb505df518cd7a1
Qinghai Meng <mengqhai@gmail.com>
2014-02-19 21:21:53 +0800

    send rich message

--- appBasement/src/main/java/com/appbasement/email/EmailService.java
+++ appBasement/src/main/java/com/appbasement/email/EmailService.java
@@ -17,9 +17,31 @@ public class EmailService implements IEmailService {
 	@Autowired
 	protected JavaMailSender mailSender;
 
+	protected void checkInvalidArgs(String from, String to, String subject,
+			String content) throws IllegalArgumentException {
+		if (from == null || "".equals(from)) {
+			throw new IllegalArgumentException("From is null or empty: " + from);
+		}
+
+		if (to == null || to.equals("")) {
+			throw new IllegalArgumentException("To is null or empty: " + to);
+		}
+
+		if (subject == null || subject.equals("")) {
+			throw new IllegalArgumentException("Content is null or empty: "
+					+ content);
+		}
+
+		if (content == null || content.equals("")) {
+			throw new IllegalArgumentException("Content is null: " + content);
+		}
+	}
+
 	@Override
 	public void sendSimpleEmail(String from, String to, String subject,
 			String content) {
+		checkInvalidArgs(from, to, subject, content);
+
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setFrom(from);
 		message.setTo(to);
@@ -31,6 +53,8 @@ public class EmailService implements IEmailService {
 	@Override
 	public void sendRichEmail(String from, String to, String subject,
 			String richContent) throws EmailSendingException {
+		checkInvalidArgs(from, to, subject, richContent);
+
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper;
 		try {
--- appBasement/src/test/java/com/appbasement/email/EmailServiceTest.java
+++ appBasement/src/test/java/com/appbasement/email/EmailServiceTest.java
@@ -1,20 +1,31 @@
 package com.appbasement.email;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static junitparams.JUnitParamsRunner.$;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
 
+@RunWith(JUnitParamsRunner.class)
 public class EmailServiceTest {
 
 	protected static Wiser wiser;
@@ -45,27 +56,107 @@ public class EmailServiceTest {
 		wiser.getMessages().clear();
 	}
 
-	@Test
-	public void testSendSimpleMessage() throws Exception {
+	protected Object[] getSimpleMessageArguments() {
 		String from = "testfrom@mail.com";
 		String to = "testto@mail.com";
 		String subject = "test mail subject";
 		String content = "Hello there!";
+		// from & subject can be null
+		return $($(from, to, subject, content));
+	}
+
+	@Test
+	@Parameters(method = "getSimpleMessageArguments")
+	public void testSendSimpleMessage(String from, String to, String subject,
+			String content) throws Exception {
 		emailService.sendSimpleEmail(from, to, subject, content);
 		List<WiserMessage> mList = wiser.getMessages();
 		assertEquals(1, mList.size());
 		WiserMessage m = mList.get(0);
-		assertEquals(from, m.getEnvelopeSender());
+
+		if (from != null) {
+			assertEquals(from, m.getEnvelopeSender());
+		}
+
 		assertEquals(to, m.getEnvelopeReceiver());
 
 		MimeMessage mm = m.getMimeMessage();
 		assertEquals(subject, mm.getSubject());
 		assertEquals(content, mm.getContent().toString().trim());
+		assertEquals("text/plain; charset=us-ascii", mm.getContentType());
+	}
+
+	protected Object[] getSimpleMessageInvalidArguments() {
+		String from = "testfrom@mail.com";
+		String to = "testto@mail.com";
+		String subject = "test mail subject";
+		String content = "Hello there!";
+		return $($(from, null, subject, content), $(from, to, subject, null),
+				$("", to, subject, content), $(from, "", subject, content),
+				$(null, to, subject, content), $(from, to, null, content),
+				$(from, to, subject, ""), $(from, to, "", content));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method = "getSimpleMessageInvalidArguments")
+	public void testSendSimpleMessageInvalidArguments(String from, String to,
+			String subject, String content) throws Exception {
+		emailService.sendSimpleEmail(from, to, subject, content);
+	}
+
+	protected Object[] getRichMessageInvalidArguments() {
+		String from = "testfrom@mail.com";
+		String to = "testto@mail.com";
+		String subject = "test mail subject";
+		String content = "<html><body><h4>Hello there!</h4></body></html>";
+		return $($(from, null, subject, content),
+				$(from, "", subject, content), $(from, to, subject, null),
+				$("", to, subject, content), $(null, to, subject, content),
+				$(from, to, null, content));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method = "getRichMessageInvalidArguments")
+	public void testSendRichMessageInvalidArguments(String from, String to,
+			String subject, String content) {
+		emailService.sendRichEmail(from, to, subject, content);
+	}
+
+	protected Object[] getRichMessageArguments() {
+		String from = "testfrom@mail.com";
+		String to = "testto@mail.com";
+		String subject = "test mail subject";
+		String content = "<html><body><h4>Hello there!</h4></body></html>";
+		return $($(from, to, subject, content));
 	}
 
 	@Test
-	public void testOther() {
-		assertTrue(wiser.getMessages().isEmpty());
-	}
+	@Parameters(method = "getRichMessageArguments")
+	public void testSendRichMessage(String from, String to, String subject,
+			String content) throws Exception {
+		emailService.sendRichEmail(from, to, subject, content);
 
+		List<WiserMessage> mList = wiser.getMessages();
+		assertEquals(1, mList.size());
+		WiserMessage m = mList.get(0);
+		MimeMessage mm = m.getMimeMessage();
+		assertTrue(mm.getContentType().startsWith("multipart/mixed;"));
+		assertTrue(mm.getContentType().contains("boundary="));
+		assertTrue(mm.getContent() instanceof MimeMultipart);
+		MimeMultipart part = (MimeMultipart) mm.getContent();
+
+		assertTrue(part.getBodyPart(0) instanceof MimeBodyPart);
+		MimeBodyPart body = (MimeBodyPart) part.getBodyPart(0);
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				body.getInputStream()));
+		String line = reader.readLine();
+		while (line != null) {
+			if (line.equals(content))
+				break;
+
+			line = reader.readLine();
+		}
+		assertEquals(content, line);
+	}
 }
