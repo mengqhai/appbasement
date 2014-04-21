b8a300f8873069afe64628d56b33bc91b5d63b3a
Qinghai Meng <mengqhai@gmail.com>
2014-04-21 16:14:29 +0800

    get user

--- angularJsPlay/src/main/java/com/angularjsplay/mvc/rest/UserController.java
+++ angularJsPlay/src/main/java/com/angularjsplay/mvc/rest/UserController.java
@@ -1,9 +1,12 @@
 package com.angularjsplay.mvc.rest;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +15,22 @@ import com.appbasement.model.User;
 import com.appbasement.service.user.IAppUserService;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping(value = "/user", headers = "Accept=application/json")
 public class UserController {
 
 	@Autowired
 	IAppUserService userService;
 
-	@RequestMapping(method = RequestMethod.GET, headers = "Accept=application/json")
-	public @ResponseBody List<User> listUsers() {
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody
+	List<User> listUsers() {
 		return userService.getAllUsers();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public @ResponseBody User getUser(@PathVariable("id") long id) {
+
+		return null;
 	}
 
 }
