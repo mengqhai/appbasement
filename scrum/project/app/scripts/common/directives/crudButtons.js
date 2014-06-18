b5d4192c42e9d6e593c9e6de3d06c3f240d731df
Qinghai Meng <mengqhai@gmail.com>
2014-06-18 16:26:56 +0800

    fix button alignment

--- scrum/project/app/scripts/common/directives/crudButtons.js
+++ scrum/project/app/scripts/common/directives/crudButtons.js
@@ -3,7 +3,7 @@ angular.module('directives.crud.buttons', [])
         return {
             restrict: 'E',
             replace: true,
-            template:'<div>' +
+            template:'<div class="btn-toolbar">' +
                 '<button type="button" class="btn btn-primary" ng-disabled="!canSave()" ng-click="save()">Save</button> ' +
                 '<button type="button" class="btn btn-warning" ng-disabled="!canRevert()" ng-click="revertChanges()">Revert changes</button> '+
                 '<button type="button" class="btn btn-danger" ng-disabled="!canDelete()" ng-click="delete()">Delete</button>'+
