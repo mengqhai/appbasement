305b90254ca03ced77069ce91623553a07371766
Qinghai Meng <mengqhai@gmail.com>
2014-06-19 16:20:16 +0800

    crud buttons alignment with col-md-offset-x

--- scrum/project/app/scripts/common/directives/crudButtons.js
+++ scrum/project/app/scripts/common/directives/crudButtons.js
@@ -3,10 +3,6 @@ angular.module('directives.crud.buttons', [])
         return {
             restrict: 'E',
             replace: true,
-            template:'<div class="btn-toolbar">' +
-                '<button type="button" class="btn btn-primary" ng-disabled="!canSave()" ng-click="save()">Save</button> ' +
-                '<button type="button" class="btn btn-warning" ng-disabled="!canRevert()" ng-click="revertChanges()">Revert changes</button> '+
-                '<button type="button" class="btn btn-danger" ng-disabled="!canDelete()" ng-click="delete()">Delete</button>'+
-                '</div>'
+            templateUrl: 'views/common/directives/crudButtons.html'
         }
     });
