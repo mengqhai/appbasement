78ed1659e11b9022daf1a27221d8ae8605d0e7b7
Qinghai Meng <mengqhai@gmail.com>
2014-07-23 14:34:28 +0800

    color picker with radio

--- uiComponents/project/app/demo/nice-inputs/demo.js
+++ uiComponents/project/app/demo/nice-inputs/demo.js
@@ -1,7 +1,10 @@
-angular.module('nice-checkbox-demo',[])
-    .controller('DemoCtrl', function($scope) {
-        $scope.selected1=true;
-        $scope.selected2=false;
-        $scope.selected3=false;
+angular.module('nice-checkbox-demo', [])
+    .controller('DemoCtrl', function ($scope) {
+        $scope.selected1 = true;
+        $scope.selected2 = false;
+        $scope.selected3 = false;
         $scope.selectedOption = 'Option1';
+        $scope.colorPicked = {value:"red"};
+        $scope.colors = [null, 'brown', 'orange', 'yellow', 'green-1', 'green-2', 'blue-1', 'blue-2', 'blue-3',
+            'violet', 'rose', 'red'];
     });
