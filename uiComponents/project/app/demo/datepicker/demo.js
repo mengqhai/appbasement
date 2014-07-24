0da6377161f3f4845dc7494f0ed59f384310dd98
Qinghai Meng <mengqhai@gmail.com>
2014-07-24 15:36:24 +0800

    dropdown popup + datepicker panel

--- uiComponents/project/app/demo/datepicker/demo.js
+++ uiComponents/project/app/demo/datepicker/demo.js
@@ -1,10 +1,18 @@
-angular.module('datepicker-demo',['ui.bootstrap','ui.bootstrap.datepicker', 'components.datepicker-panel'])
+angular.module('datepicker-demo',['ui.bootstrap','ui.bootstrap.datepicker', 'components.datepicker-panel',
+        'components.dropdown-popup'])
     .controller('DemoCtrl', function($scope) {
 
         $scope.date1 = new Date();
         $scope.dateInfo = {
             date: new Date(),
             urgent: false
+        };
+        $scope.popupInfo = {
+            isOpened:false
+        };
+
+        $scope.toggleOpen = function() {
+            $scope.popupInfo.isOpened = !$scope.popupInfo.isOpened;
         }
 
     });
--- uiComponents/project/app/scripts/components/dropdown-popup.js
+++ uiComponents/project/app/scripts/components/dropdown-popup.js
@@ -20,22 +20,13 @@ angular.module('components.dropdown-popup', [])
 
                 var doOpen = function (opened) {
                     if (opened) {
-                        besideToggle(scope.toggle);
                         ele.addClass('is-opened');
                         $document.bind('click', docClickHandler);
                     } else {
                         ele.removeClass('is-opened');
                         $document.unbind('click', docClickHandler);
                     }
-                }
-                var besideToggle = function(tEle) {
-                    if (!tEle) {
-                        return;
-                    }
-                    var th = tEle.css('height');
-                    var tw = tEle.css('width');
-                    console.info(th+" "+tw);
-                }
+                };
                 doOpen(popupInfo.isOpened);
                 scope.$watch('popupInfo.isOpened', function (opened) {
                     doOpen(opened);
@@ -45,11 +36,17 @@ angular.module('components.dropdown-popup', [])
                     if(!popupInfo.isOpened) {
                         return;
                     }
-                    var tEle = angular.element(e.target);
-                    if (tEle.attr('toggle-popup') === ele.attr('id')) {
-                        scope.toggle=tEle;
+//                    var tEle = angular.element(e.target);
+//                    if (tEle.attr('toggle-popup') && tEle.attr('toggle-popup') === ele.attr('id')) {
+//                        scope.toggle=tEle;
+//                        return;
+//                    }
+                    var popupParent = ele.parent()[0];
+                    var tParent = angular.element(e.target).parent()[0];
+                    if (tParent === popupParent) {
                         return;
                     }
+
                     scope.$apply(function () {
                         popupInfo.isOpened = false;
                     })
