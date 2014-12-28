080b53f84f58b99be3c8fc24bf3239adcce5ed7c
Qinghai Meng <mengqhai@gmail.com>
2014-12-28 14:06:09 +0800

    template service and test

--- workstream-core/src/main/java/com/workstream/core/service/TemplateService.java
+++ workstream-core/src/main/java/com/workstream/core/service/TemplateService.java
@@ -1,5 +1,14 @@
 package com.workstream.core.service;
 
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,15 +16,61 @@ import org.springframework.transaction.annotation.Transactional;
 import com.workstream.core.CoreConstants;
 
 /**
- * Process templated related service.
+ * Process template(deployment & process definitions) related service.
  * 
  * @author qinghai
- *
+ * 
  */
 @Service
 @Transactional(propagation = Propagation.REQUIRED, value = CoreConstants.TX_MANAGER)
 public class TemplateService {
-	
-	
+
+	@Autowired
+	private RepositoryService repoSer;
+
+	public Deployment createDeployment(Long orgId, String fileName,
+			InputStream in) {
+		DeploymentBuilder deploymentBuilder = repoSer.createDeployment()
+				.name(fileName).tenantId(String.valueOf(orgId));
+		if (fileName.endsWith(".bpmn20.xml") || fileName.endsWith(".bpmn")) {
+			deploymentBuilder.addInputStream(fileName, in);
+		} else if (fileName.endsWith(".bar") || fileName.endsWith(".zip")) {
+			deploymentBuilder.addZipInputStream(new ZipInputStream(in));
+		} else {
+			throw new RuntimeException(
+					"Unable to deside the deployment type, have you correctly set the file extension? "
+							+ fileName);
+		}
+		return deploymentBuilder.deploy();
+	}
+
+	public Deployment getDeployment(String deploymentId) {
+		return repoSer.createDeploymentQuery().deploymentId(deploymentId)
+				.singleResult();
+	}
+
+	public List<Deployment> filterDeployment(Long orgId) {
+		return repoSer.createDeploymentQuery()
+				.deploymentTenantId(String.valueOf(orgId)).list();
+	}
+
+	/**
+	 * non-cascading deletion
+	 * 
+	 * @param deploymentId
+	 */
+	public void removeDeployment(String deploymentId) {
+		repoSer.deleteDeployment(deploymentId);
+	}
+
+	public List<ProcessDefinition> filterProcessTemplate(Long orgId) {
+		return repoSer.createProcessDefinitionQuery()
+				.processDefinitionTenantId(String.valueOf(orgId)).list();
+	}
+
+	public List<ProcessDefinition> filterProcessTemplate(String deploymentId) {
+		return repoSer.createProcessDefinitionQuery()
+				.deploymentId(deploymentId).list();
+	}
 
 }
---
+++ workstream-core/src/test/java/com/workstream/core/service/TemplateServiceTest.java
@@ -0,0 +1,102 @@
+package com.workstream.core.service;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.workstream.core.CoreConstants;
+import com.workstream.core.conf.ApplicationConfiguration;
+import com.workstream.core.model.Organization;
+import com.workstream.core.model.UserX;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationConfiguration.class)
+@TransactionConfiguration(transactionManager = CoreConstants.TX_MANAGER, defaultRollback = false)
+public class TemplateServiceTest {
+
+	@Autowired
+	RepositoryService repoSer;
+
+	@Autowired
+	RuntimeService ruSer;
+
+	@Autowired
+	private CoreFacadeService core;
+
+	@Autowired
+	private IdentityService idService;
+
+	@Autowired
+	private TaskService taskSer;
+
+	String userId = "templateServiceTester@sina.com";
+
+	String orgIdentifier = "templateServiceTestOrg";
+
+	Organization org;
+
+	@Autowired
+	private TemplateService temSer;
+
+	@Before
+	@Transactional(value = CoreConstants.TX_MANAGER, propagation = Propagation.REQUIRED)
+	public void before() {
+		TestUtils
+				.clearUser(userId, core.getUserService(), core.getOrgService());
+		TestUtils.clearOrg(orgIdentifier, core);
+		TestUtils.clearOrphanGroups(idService);
+		core.getUserService().createUser(userId, "User to join org", "123");
+		UserX userX = core.getUserService().getUserX(userId);
+		org = core.createInitOrg(userX, "Template Service Test Org",
+				orgIdentifier, null);
+	}
+
+	@Test
+	public void testCreateDeployment() {
+		String fileName = "TestProcess1.bpmn";
+		InputStream bpmnIn = this.getClass().getResourceAsStream(
+				"/process/" + fileName);
+		Deployment deploy = temSer.createDeployment(org.getId(), fileName,
+				bpmnIn);
+		Assert.assertNotNull(deploy.getId());
+		Assert.assertEquals(String.valueOf(org.getId()), deploy.getTenantId());
+
+		List<Deployment> deployList = temSer.filterDeployment(org.getId());
+		Assert.assertEquals(1, deployList.size());
+		Assert.assertEquals(deploy.getId(), deployList.get(0).getId());
+		Assert.assertEquals(String.valueOf(org.getId()), deployList.get(0)
+				.getTenantId());
+
+		List<ProcessDefinition> pdList = temSer.filterProcessTemplate(deploy
+				.getId());
+		Assert.assertEquals(1, pdList.size());
+		List<ProcessDefinition> pdList1 = temSer.filterProcessTemplate(org
+				.getId());
+		Assert.assertEquals(1, pdList1.size());
+
+		// test delete
+		temSer.removeDeployment(deploy.getId());
+		deployList = temSer.filterDeployment(org.getId());
+		Assert.assertEquals(0, deployList.size());
+		pdList1 = temSer.filterProcessTemplate(org.getId());
+		Assert.assertEquals(0, pdList1.size());
+	}
+
+}
