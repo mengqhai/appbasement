543bc8c272611bcfe2cad22d01fd8702cac03f15
Qinghai Meng <mengqhai@gmail.com>
2015-02-12 20:33:20 +0800

    task dialog

--- workstream-ui/app/scripts/controllers/tasks.js
+++ workstream-ui/app/scripts/controllers/tasks.js
@@ -12,11 +12,33 @@ angular.module('controllers.tasks', ['resources.tasks', 'ui.router'])
 
         //$scope.loadCounts();
     }])
-    .controller('TaskListController', ['$scope', 'Tasks', '$stateParams', function ($scope, Tasks, $stateParams) {
+    .controller('TaskListController', ['$scope', 'Tasks', '$stateParams', '$modal', function ($scope, Tasks, $stateParams, $modal) {
         $scope.params = $stateParams;
         $scope.tasks = [];
         $scope.loadByType = function () {
             $scope.tasks = Tasks.getByListType({type: $stateParams.listType});
         };
         $scope.loadByType();
+
+        var dialog = null;
+        $scope.openDialog = function (task) {
+            dialog = $modal.open({
+                templateUrl: 'views/taskForm.html',
+                controller: 'TaskFormController',
+                resolve: {
+                    task: function () {
+                        return task;
+                    }
+                },
+                scope: $scope
+            })
+        };
+        $scope.closeDialog = function () {
+            if (dialog) {
+                dialog.dismiss('cancelTask');
+            }
+        }
+    }])
+    .controller('TaskFormController', ['$scope', 'Tasks', 'task', function ($scope, Tasks, task) {
+        $scope.task = task;
     }]);
