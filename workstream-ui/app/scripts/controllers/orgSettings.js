20b409e80eda6584944c7790067cf3a225fb5fb0
qinghai <mengqhai@gmail.com>
2015-06-01 22:46:26 +0800

    enabled removing/adding user from/to groups in UI

--- workstream-ui/app/scripts/controllers/orgSettings.js
+++ workstream-ui/app/scripts/controllers/orgSettings.js
@@ -27,23 +27,25 @@ angular.module('controllers.orgSettings', ['controllers.groups'])
         function ($scope, Orgs, Groups, Users, $stateParams, $modal) {
             $scope.groups = Orgs.getGroupsInOrg({orgId: $stateParams.orgId, withDetails: true});
             $scope.groupMembers = {};
-            $scope.ungroupedUsers = Orgs.getUsersInOrg({orgId: $stateParams.orgId});
-
-            var updateUngroupedUsers = function() {
+            var initUngroupedUsers = function() {
+                $scope.ungroupedUsers = Orgs.getUsersInOrg({orgId: $stateParams.orgId});
+            };
+            initUngroupedUsers();
+            var updateUngroupedUsers = function () {
                 for (var i = 0; i < $scope.groups.length; i++) {
                     var theMembers = Groups.getMembers({groupId: $scope.groups[i].groupId});
                     $scope.groupMembers[$scope.groups[i].groupId] = theMembers;
-                    theMembers.$promise.then(function(arr) {
-                        for (var i =0;i<arr.length;i++) {
+                    theMembers.$promise.then(function (arr) {
+                        for (var i = 0; i < arr.length; i++) {
                             var matchIdx = -1;
-                            for (var j =0; j<$scope.ungroupedUsers.length;j++) {
+                            for (var j = 0; j < $scope.ungroupedUsers.length; j++) {
                                 if (arr[i].id === $scope.ungroupedUsers[j].id) {
                                     // this is a grouped user, need to be removed
                                     matchIdx = j;
                                     break;
                                 }
                             }
-                            if (matchIdx!==-1) {
+                            if (matchIdx !== -1) {
                                 $scope.ungroupedUsers.splice(matchIdx, 1);
                             }
                         }
@@ -78,15 +80,15 @@ angular.module('controllers.orgSettings', ['controllers.groups'])
                     var g = $scope.groups[i];
                     var groupId = g.groupId;
                     var members = $scope.groupMembers[groupId];
-                    var isMember =false;
-                    for (var j = 0; j< members.length; j++) {
+                    var isMember = false;
+                    for (var j = 0; j < members.length; j++) {
                         var m = members[j];
                         if (m.id === user.id) {
                             isMember = true;
                             break;
                         }
                     }
-                    if(!isMember) {
+                    if (!isMember) {
                         availableGroups.push(g);
                     }
 
@@ -102,14 +104,16 @@ angular.module('controllers.orgSettings', ['controllers.groups'])
             $scope.deleteGroupMember = function (group, user) {
                 Groups.deleteMember({groupId: group.groupId, userIdBase64: btoa(user.id)}).$promise.then(function () {
                     console.log('deleted user ' + user.id + ' from group ' + group.groupId);
-                    var members = $scope.groupMembers[group.groupId];
-                    var idx = members.indexOf(user);
-                    members.splice(idx, 1);
+//                    var members = $scope.groupMembers[group.groupId];
+//                    var idx = members.indexOf(user);
+//                    members.splice(idx, 1);
+                    initUngroupedUsers();
+                    updateUngroupedUsers();
                 });
             };
-            $scope.addGroupMember = function(group, user) {
-                Groups.addMember({groupId: group.groupId, userIdBase64: btoa(user.id)}).$promise.then(function() {
-                    console.log('added user '+user.id+' to group '+group.groupId);
+            $scope.addGroupMember = function (group, user) {
+                Groups.addMember({groupId: group.groupId, userIdBase64: btoa(user.id)}, null).$promise.then(function () {
+                    console.log('added user ' + user.id + ' to group ' + group.groupId);
                     updateUngroupedUsers();
                 })
             }
