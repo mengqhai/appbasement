b0f06ff7a111865a7970bc5eafae4fd5f00a8414
Qinghai Meng <mengqhai@gmail.com>
2015-06-12 15:00:12 +0800

    enabled to undeploy the template from the UI

--- workstream-ui/app/scripts/controllers/templates.js
+++ workstream-ui/app/scripts/controllers/templates.js
@@ -1,28 +1,44 @@
 angular.module('controllers.templates', ['resources.templates'])
-    .controller('TemplateListController', ['$scope', 'Templates', 'Orgs', 'templates', function($scope, Templates, Orgs, templates) {
+    .controller('TemplateListController', ['$scope', 'Templates', 'Orgs', 'templates', function ($scope, Templates, Orgs, templates) {
         $scope.templates = templates;
     }])
-    .controller('TemplateDetailsController', ['$scope', '$stateParams', 'templates', 'Templates', function($scope, $stateParams, templates, Templates) {
-        function getTemplate(templates) {
-            for (i=0;i<templates.length;i++) {
-                var template = templates[i];
-                if ($stateParams.templateId === template.id) {
-                    $scope.template = template;
-                    return template;
+    .controller('TemplateDetailsController', ['$scope', '$stateParams', 'templates', 'Templates', '$state',
+        function ($scope, $stateParams, templates, Templates, $state) {
+            function getTemplate(templates) {
+                for (i = 0; i < templates.length; i++) {
+                    var template = templates[i];
+                    if ($stateParams.templateId === template.id) {
+                        $scope.template = template;
+                        return template;
+                    }
                 }
             }
-        }
-        if (templates.$resolved) {
-            getTemplate(templates);
-        } else {
-            templates.$promise.then(getTemplate);
-        }
 
-        $scope.getDiagramUrl = function(templateId) {
-            if (!$scope.template) {
-                return null;
+            if (templates.$resolved) {
+                getTemplate(templates);
             } else {
-                return Templates.getDiagramUrl(templateId);
+                templates.$promise.then(getTemplate);
             }
-        }
-    }]);
+
+            $scope.getDiagramUrl = function (templateId) {
+                if (!$scope.template) {
+                    return null;
+                } else {
+                    return Templates.getDiagramUrl(templateId);
+                }
+            }
+
+            $scope.stateObj = {
+                isUndeployConfirmOpen: false
+            };
+
+            /** undeploy **/
+            $scope.undeployTemplate = function () {
+                Templates.delete({templateId: $scope.template.id}, function () {
+                    $state.reload().then(function () {
+                        $state.go('^');
+                    });
+                    $scope.template = null;
+                });
+            }
+        }]);
