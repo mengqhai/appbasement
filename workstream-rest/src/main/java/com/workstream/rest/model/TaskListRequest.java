70e774e7cb3c71e49407ffdb5cf3df2fac2b12b4
Qinghai Meng <mengqhai@gmail.com>
2015-07-07 12:16:00 +0800

    added validation for task list creation

--- workstream-rest/src/main/java/com/workstream/rest/model/TaskListRequest.java
+++ workstream-rest/src/main/java/com/workstream/rest/model/TaskListRequest.java
@@ -2,12 +2,20 @@ package com.workstream.rest.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.workstream.rest.RestConstants;
+import com.workstream.rest.validation.ValidateOnCreate;
+
 public class TaskListRequest extends MapPropObj {
 
 	public void setName(String name) {
 		this.props.put(NAME, name);
 	}
 
+	@NotNull(groups = ValidateOnCreate.class)
+	@Size(max = RestConstants.VALID_NAME_SIZE, min = 1)
 	public String getName() {
 		return this.getProp(NAME);
 	}
@@ -16,6 +24,7 @@ public class TaskListRequest extends MapPropObj {
 		this.props.put(DESCRIPTION, description);
 	}
 
+	@Size(max = RestConstants.VALID_DESCRIPTION_SIZE, min = 1)
 	public String getDescription() {
 		return this.getProp(DESCRIPTION);
 	}
